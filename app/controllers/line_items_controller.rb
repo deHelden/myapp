7803fd6d44f0639105059ea5d099affbdd26df64
Vladimir <vovkati3@gmail.com>
2019-04-16 22:42:37 +0300

    First commit

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,7 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+  before_action :set_line_items, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
   # GET /line_items.json
@@ -25,6 +27,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     @line_item = LineItem.new(line_item_params)
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
